b62acf5fbf529955426e07c9d5f778843512c033
mubaraklouis@gmail.com <mubaraklouis@gmail.com>
2024-11-02 17:51:59 +0200

    make the mobile menue responsive

--- resources/js/customComponenents/navbar.vue
+++ resources/js/customComponenents/navbar.vue
@@ -7,8 +7,30 @@ import primaryButton from './primary-button.vue';
             <div>
           MALGA
             </div>
-       <div class="hidden">
-        <ul>
+
+
+            <div class="menu-mobile-visibility">
+        <ul class="flex text-bold gap-4 ">
+            <li class="text-extrabold">
+                Home
+            </li>
+            <li>
+                About
+            </li>
+            <li>
+                Contact
+            </li>
+            <li>
+                Features
+            </li>
+        </ul>
+       </div>
+            <div class="primary-btn-visibility">
+<primaryButton title="Subscribe"/>
+            </div>
+
+       <div class=" laptop-size flex  bg-[#0c093d] absolute  items-center w-full h-[50vh] top-20 left-0 text-white justify-center font-bold">
+        <ul class="grid grid-cols-1 gap-6 items-center">
                 <li>
                     Home
                 </li>
@@ -22,21 +44,44 @@ import primaryButton from './primary-button.vue';
                 <li>
                     Pages
                 </li>
+                <div>
+                    <primaryButton title="Subscribe"/>
+                </div>
             </ul>
        </div>
 
-       <div>
+       <div class="hand-menu">
         <img class="bars-size" src="/icons/bars.png" alt="">
        </div>
-            <div class="hidden">
-<primaryButton title="Subscribe"/>
-            </div>
+
+
 
         </div>
 
     </nav>
 </template>
+
+
 <style scoped>
+
+@media (min-width: 1024px) and (max-width: 1440px) {
+.laptop-size{
+    display: none;
+}
+.hand-menu{
+    display:none;
+}
+
+}
+
+@media (max-width: 768px){
+.primary-btn-visibility{
+    display: none;
+}
+.menu-mobile-visibility{
+display:none;
+}
+}
 .logo-size{
     width:150px;
     height: 150px;
