f5a14dcde7a1ea4b5c2cdd884a8af164b2282fff
mubaraklouis@gmail.com <mubaraklouis@gmail.com>
2024-11-02 12:50:18 +0200

    make the primary buttom

---
+++ resources/js/Pages/heroSection.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<section>
+    <div>
+        <h1>
+            Design Your Dream
+        </h1>
+
+        <div>
+            <p>
+                Get custom T-shirts and Eye-catching flyers and More!
+                elevate your brand with custom Tees,Graphic Design and Trading solutions
+            </p>
+
+            <div>
+                <button>Explorer More</button>
+            </div>
+
+        </div>
+    </div>
+</section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/customComponenents/navbar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+</script>
+<template>
+    <nav>
+        <div>
+            <div>
+                Logo
+            </div>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>   <li>
+                Contact
+            </li>   <li>
+                Features
+            </li>
+                <li>
+                    Pages
+                </li>
+            </ul>
+
+        </div>
+
+        <!-- working on the primary button -->
+   <div class="primary-button">
+    subscribe
+   </div>
+    </nav>
+</template>
+<style scoped>
+.primary-button{
+    background-color:var(--secondaryColor);
+    color:#fff;
+    padding:5px 20px;
+    width:fit-content;
+    border-radius:20px;
+
+}
+
+</style>
