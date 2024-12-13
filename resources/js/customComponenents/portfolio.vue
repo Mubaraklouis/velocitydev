6b60e39c065d8ca3ea7f7b2a91277b68f7b71b16
mubarak <mubaraklouis@velocity.com>
2024-12-13 22:00:32 +0200

    prepare the glows

--- resources/js/customComponenents/portfolio.vue
+++ resources/js/customComponenents/portfolio.vue
@@ -12,7 +12,10 @@ import girlGlow from './girlGlow.vue';
     <h1 class="mt-10 mb-10 text-4xl font-extrabold text-center text-extrabold">Our portfolio</h1>
 
     <div>
-        
+        <div>
+
+        </div>
+
     </div>
 
 
@@ -24,22 +27,6 @@ import girlGlow from './girlGlow.vue';
 
 <style scoped>
 
-/* Media query for laptop screens */
-@media screen and (min-width: 1024px) and (max-width: 1440px) {
-    .portfolio-wrapper{
-        margin-top: 20px;
-        margin-bottom: 20px;
-        display:grid;
-        grid-template-columns: 1fr 1fr;
-    }
-    .girl-image{
-        width:400px;
-        /* height: 200px; */
-    }
-
-
-
-}
 
 
 </style>
