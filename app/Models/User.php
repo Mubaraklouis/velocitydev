4a600b46bd7b435d5955633c716557eb95ac1aeb
mubarak <luismubarak@gmail.com>
2025-02-22 22:51:47 +0200

    send some updates to the code

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon;
+
+
 
 class User extends Authenticatable
 {
