9b0ee1659762b68c2d0fcab3f440e324fe80bcef
mubarak <luismubarak@gmail.com>
2025-02-23 13:41:06 +0200

    make the fillable to avoid mass assigment

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
+    protected $fillable = ['title', 'description', 'image'];
+    
     /** @use HasFactory<\Database\Factories\ServiceFactory> */
     use HasFactory;
 }
