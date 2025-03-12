a621b61b7428ea53109ee72d2d61a83b14ae8650
mubarak <luismubarak@gmail.com>
2025-03-12 17:41:07 +0200

    trying to fix the bug

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -29,7 +29,7 @@ const logout = () => {
                 <nav class="flex-1 px-2 py-4 bg-gray-800">
 
 
-                    <Link href="/dashboard/notifications"
+                    <Link :href="route('notification.index')"
                         class="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700">
 
                         <img class="w-5 h-5 mr-3 " src="/notification.png"/>
